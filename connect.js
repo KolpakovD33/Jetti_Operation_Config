5e5809fff942410077ab88fd736e16877680f6fc
KolpakovD33 <Kolpakov.D33@live.com>
2021-03-06 10:55:28 +0200

    test connestions

--- connect.js
+++ connect.js
@@ -1,33 +1,3 @@
-var sql = require('mssql');
+let str = require('./settings.js');
 
-sql.connect("mssql://PowerUser:13Cb1KdpiHxK@sql.jetti-app.com/sm").then(function () {
-  // Query
-
-  // new sql.Request().query('select * from mytable').then(function(recordset) {
-  // 	console.dir(recordset);
-  // }).catch(function(err) {
-  // 	// ... query error checks
-  // });
-
-  //   // Stored Procedure
-
-  // new sql.Request()
-  // .input('input_parameter', sql.Int, value)
-  //   .output('output_parameter', sql.VarChar(50))
-  // .execute('procedure_name').then(function(recordsets) {
-  // 	console.dir(recordsets);
-  // }).catch(function(err) {
-  // 	// ... execute error checks
-  // });
-
-  // ES6 Tagged template literals (experimental)
-
-  sql.query`SELECT TOP 3 pc.*
-            FROM [dbo].[Catalog.Operation.v] as pc with (noexpand)`.then(function (recordset) {
-    console.dir(recordset);
-  }).catch(function (err) {
-    // ... query error checks
-  });
-}).catch(function (err) {
-  // ... connect error checks
-});
+console.log(str);
