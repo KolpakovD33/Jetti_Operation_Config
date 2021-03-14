402f36b12ef7d61df2cb2e4379354e007af5b26b
KolpakovD33 <Kolpakov.D33@live.com>
2021-03-15 01:44:13 +0200

    JSON.parse

--- Server/Get.Catalog.Operation.ts
+++ Server/Get.Catalog.Operation.ts
@@ -6,8 +6,18 @@ connection.on('connect', async function (err) {
     if (err) {
         console.log(err);
     } else {
+        let resultArray: any[] = [];
         const ttt: any[] = await manyOrNone(sql)
-        console.log(ttt);
+        for (let row of ttt) {
+            resultArray.push({
+                Code: row.code,
+                Name: row.description,
+                doc: JSON.parse(row.doc)
+                // Script: JSON.parse(row.doc.script),
+                // Parameters: JSON.parse(row.doc.Parameters)
+            })
+        };
+        console.log(resultArray);
         connection.close();
     }
 });
